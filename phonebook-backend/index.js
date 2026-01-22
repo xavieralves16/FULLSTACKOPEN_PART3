47b4ae8f02ff55adb549496f88cdc76d94079a80
Xavier <xavieralves162@gmail.com>
2026-01-22 15:59:33 +0000

    Exc 3.6

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -68,12 +68,25 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  // Missing name or number
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number missing'
     })
   }
 
+  // Name must be unique
+  const nameExists = persons.some(
+    person => person.name === body.name
+  )
+
+  if (nameExists) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  // Create new person
   const person = {
     id: generateId(),
     name: body.name,

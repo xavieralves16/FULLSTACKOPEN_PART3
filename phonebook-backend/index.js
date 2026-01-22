2508e49322467be71ddeba0604aa6a1c76de89fc
Xavier <xavieralves162@gmail.com>
2026-01-22 16:29:48 +0000

    Exc 3.8

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
+
 
 let persons = [
   { 
